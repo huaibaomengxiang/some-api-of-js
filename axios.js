35dcd2a211a626a685bbf6164152cc5e97354684
huaibaomengxiang <764297325@qq.com>
2018-04-19 17:31:08 +0800

    '修改axios'

--- axios.js
+++ axios.js
@@ -4,14 +4,14 @@ import {encrypt} from '../ase/index'
 // import store from '@/store'
 // import { getToken } from '@/utils/auth'
 
-const publicData={accountId:32,token:'0a2ef4df6685f4e85dd95e7428a74721',source:1}
-const baseUrl='http://192.168.1.191:8080/juhaowan'
+const publicData = {accountId: 32, token: 'b14fad089098ac82f8cf9695f28d8380', source: 1}
+const baseUrl = 'http://192.168.1.191:8080/juhaowan'
 // create an axios instance
 const service = axios.create({
     baseURL: baseUrl, // api的base_url
     timeout: 5000, // request timeout
     responseType: "json",
-    withCredentials: true, // 是否允许带cookie这些
+    // withCredentials: true, // 是否允许带cookie这些
     headers: {
         "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
     }
@@ -19,8 +19,6 @@ const service = axios.create({
 
 // request interceptor
 service.interceptors.request.use(config => {
-
-
     return config
 }, error => {
     // Do something with request error
@@ -30,83 +28,41 @@ service.interceptors.request.use(config => {
 
 // respone interceptor
 service.interceptors.response.use(
-    response => response,
-    /**
-     * 下面的注释为通过response自定义code来标示请求状态，当code返回如下情况为权限有问题，登出并返回到登录页
-     * 如通过xmlhttprequest 状态码标识 逻辑可写在下面error中
-     */
-    //  const res = response.data;
-    //     if (res.code !== 20000) {
-    //       Message({
-    //         message: res.message,
-    //         type: 'error',
-    //         duration: 5 * 1000
-    //       });
-    //       // 50008:非法的token; 50012:其他客户端登录了;  50014:Token 过期了;
-    //       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-    //         MessageBox.confirm('你已被登出，可以取消继续留在该页面，或者重新登录', '确定登出', {
-    //           confirmButtonText: '重新登录',
-    //           cancelButtonText: '取消',
-    //           type: 'warning'
-    //         }).then(() => {
-    //           store.dispatch('FedLogOut').then(() => {
-    //             location.reload();// 为了重新实例化vue-router对象 避免bug
-    //           });
-    //         })
-    //       }
-    //       return Promise.reject('error');
-    //     } else {
-    //       return response.data;
-    //     }
-    error => {
-        console.log('err' + error)// for debug
-        return Promise.reject(error)
-    })
+    response => {
+        const res = response.data;
+        if (res.code === 104) {
+            console.log('登录时效hahah')
+            return Promise.reject('error');
+        } else {
+            return response;
+        }
+        error => {
+            console.log('err' + error)// for debug
+            return Promise.reject(error)
+        }
+    }
+)
 
 
 export default {
-    get(url,data){
-        let publicdata="{accountId:32,token:'0a2ef4df6685f4e85dd95e7428a74721',source:1}"
-        return axios({
-            method:'get',
-            url:baseUrl+url,
-            params:{data:encrypt(publicdata)},
+    get(url, data){
+        let publicdata =JSON.stringify(publicData)
+        return service({
+            method: 'get',
+            url: url,
+            params: {data: encrypt(publicdata)},
         })
-            .then(res=>res.data)
+            .then(res => res.data)
 
     },
-    post(url,data){
-        let value=Object.assign(publicData,data)
-        let Data=JSON.stringify(value)
-        return axios({
-            method:'post',
-            url:baseUrl+url,
-            params:{data:encrypt(Data)}
+    post(url, data){
+        let value = Object.assign(publicData, data)
+        let Data = JSON.stringify(value)
+        return service({
+            method: 'post',
+            url: url,
+            params: {data: encrypt(Data)}
         })
-            .then(res=>res.data)
+            .then(res => res.data)
     }
-    // get(url,data){
-    //     return new Promise((resolve,reject)=>{
-    //         axios({
-    //             method:'get',
-    //             url:baseUrl+url,
-    //             params:data,
-    //         })
-    //     })
-    //         .then(res=>{
-    //             resolve(res)
-    //         })
-    // },
-    // post(url,data){
-    //     return new Promise((resolve,reject)=>{
-    //         axios({
-    //             method:'post',
-    //             url:baseUrl+url,
-    //             params:data
-    //         })
-    //     })
-    //         .then(res=>{
-    //             resolve(res)
-    //         })
-    // }
 }
